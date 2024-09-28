af22180a8d69027e6c8f2f82ba9f3a103007085f
fatima-harun <faabintu@gmail.com>
2024-09-28 09:38:29 +0000

    second commit

---
+++ src/app/Models/user.models.ts
@@ -0,0 +1,13 @@
+export interface UserModel{
+    id?:number,
+    photo?:string,
+    nom?:string,
+    prenom?:string,
+    email?:string,
+    CNI?:string,
+    sexe?:string,
+    role?:string,
+    telephone?:string,
+    adresse?:string,
+    password?:string,
+}
